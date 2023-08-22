62cf95137647f52c2c3c56b400014694adf37092
LandyHsiao <landyhsiao776@gmail.com>
2023-08-22 23:33:53 +0800

    refactor: 💡 專案初始化

---
+++ README.md
@@ -0,0 +1,2 @@
+[Demo Page](https://landyhsiao.github.io/alphabox/)  
+[Test Page](https://landyhsiao.github.io/alphabox/test.html)  
---
+++ main.js
@@ -0,0 +1,7 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'jquery/dist/jquery.min.js';
+// import $ from 'jquery';
+// window.$ = $;
+
+console.log("Hello world!");
